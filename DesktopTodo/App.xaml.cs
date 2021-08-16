2f80912bb5c05d8fc5a53375b3a1227556ca9635
frotie <frotiecoder@gmail.com>
2021-08-16 20:47:16 +0300

    Made base structure of project

--- DesktopTodo/App.xaml.cs
+++ DesktopTodo/App.xaml.cs
@@ -13,5 +13,10 @@ namespace DesktopTodo
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ViewModelLocator.Init();
+            base.OnStartup(e);
+        }
     }
 }
---
+++ DesktopTodo/ViewModelLocator.cs
@@ -0,0 +1,29 @@
+﻿using DesktopTodo.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopTodo
+{
+    class ViewModelLocator
+    {
+        private static ServiceProvider _provider;
+        public static void Init()
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            services.AddTransient<MainViewModel>();
+
+            _provider = services.BuildServiceProvider();
+            foreach(var service in services)
+            {
+                _provider.GetRequiredService(service.ServiceType);
+            }
+        }
+
+        public MainViewModel MainViewModel => _provider.GetRequiredService<MainViewModel>();
+    }
+}
